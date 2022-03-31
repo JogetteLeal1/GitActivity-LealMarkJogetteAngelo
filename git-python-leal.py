bbd145a4a3bf4dc563389562a7d19466b8952ed2
LealMarkJogetteAngelo <jetjetleal123@gmail.com>
2022-03-31 20:32:48 +0800

    My Hobbies added

--- git-python-leal.py
+++ git-python-leal.py
@@ -2,5 +2,8 @@ print("Mark Jogette Angelo M. Leal");
 print("Murallon, Boac, Marinduque");
 print("April 1, 2002");
 print("Marinduque State College");
-print("")
-print("")
+print("Flying Drone")
+print("Riding Motorcycle")
+print("Eating")
+print("Watching")
+print("Diving")
